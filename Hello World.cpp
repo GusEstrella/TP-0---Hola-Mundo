173a2f2949a393fb329de4f709240c22209ea4fd
GusEstrella <estrellagustavo100@gmail.com>
2016-04-13 23:48:59 -0300

    Added files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,21 @@
+/* Hello World
+* JMS
+* 20130411
+*/
+
+#include <iostream>
+
+int main() {
+	
+int a,b;
+
+std::cout << "Ingrese A:";
+std::cin >> a;
+
+std::cout << "Ingrese B:";
+std::cin >> b;
+
+std::cout << "La suma entre A y B es: ";
+std::cout << a+b;
+
+}
